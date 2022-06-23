caa16a7b8560c158528823da401be8085f8696d8
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-06-23 16:34:32 +0530

    Time: 62 ms (80.13%), Space: 28.2 MB (47.65%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.cpp
+++ 561-array-partition-i/561-array-partition-i.cpp
@@ -4,7 +4,7 @@ public:
         int ans=0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i+=2){
-            ans+=min(nums[i],nums[i+1]);
+            ans+=nums[i];
         }
         return ans;
     }

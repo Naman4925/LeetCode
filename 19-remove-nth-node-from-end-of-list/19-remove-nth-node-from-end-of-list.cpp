ba5c663632bc7b6125eb1fe86fbd355e6ac6be97
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-09-25 22:21:46 +0530

    Time: 11 ms (27.19%), Space: 10.7 MB (74.97%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -8,52 +8,27 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/*class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL)
-            return head;
-        int count=0;
-        ListNode* curr=head;
-        while(curr){
-            curr=curr->next;
-            count++;
-        }
-        int k=count-n;
-        ListNode* temp=head;
-        //ListNode* prev=NULL;
-        for(int i=0;i<k-1;i++){
-            temp=temp->next;
-            //prev=prev->next;
-        }
-        temp->next=temp->next->next;
-        //delete (temp);
-        return head;
-    }
-};*/
 class Solution {
 public:
-
-    
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int sz = 0;
-        ListNode* cur = head;
-        ListNode* t = head;
-        while(t){
-            t=t->next;
-            sz++;
+        int count=0;
+        ListNode* temp=head;
+        ListNode* curr=head;
+        while(temp){
+            temp=temp->next;
+            count++;
         }
-        int i=1;
-        while(cur->next && i < sz-n)
-        {
-            cur = cur->next;
-            i++;
+        int diff=1;
+        while(curr->next && diff<count-n){
+            curr=curr->next;
+            diff++;
         }
-        if(sz-n == 0)
-            head = head->next;
-        else
-            cur->next = cur->next->next;
-            
+        if(count-n==0){
+            head=head->next;
+        }
+        else{
+            curr->next=curr->next->next;
+        }
         return head;
     }
 };

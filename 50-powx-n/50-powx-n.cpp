562c82fec12e57924cc0ed331f0cd5c54955be5e
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2022-09-27 17:23:27 +0530

    Time: 3 ms (50.65%), Space: 6.2 MB (14.97%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,17 +2,6 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double total=0;
-        // long long int pos;
-        // if(n>0){
-        //     total=pow(x,n);
-        // }
-        // else if(n<0){
-        //     pos=n*-1;
-        //     total=1/pow(x,pos);
-        // }
-        // else{
-        //     total=1;
-        // }
         total=pow(x,n);
         return total;
     }
